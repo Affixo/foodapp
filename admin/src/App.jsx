a82b1e18abae52156a3e154fdc9aa6b647d740e6
Affixo <abrarzahinayan6987@gmail.com>
2025-06-22 20:21:40 +0600

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -6,7 +6,7 @@ import Add from "./pages/Add/Add";
 
 import List from "./pages/List/List";
 import Orders from "./pages/Orders/Orders";
-import WalletAdminPanel from "./pages/Wallet/Walletadminpanel";
+import WalletAdminPanel from "./pages/Wallet/WalletAdminPanel";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 const App = () => {
