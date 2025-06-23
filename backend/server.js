6128fcada8a2743bdc778aa13a0bb4cd6849ccfe
Affixo <abrarzahinayan6987@gmail.com>
2025-06-23 23:40:47 +0600

    uploads line 40 Update server.js

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ app.use("/api/user", userRouter);
 app.use("/api/cart", cartRouter);
 app.use("/api/order", orderRouter);
 app.use("/api/admin", adminRouter);
-
+app.use('/uploads', express.static('uploads'));
 app.get("/", (req, res) => res.send("API Working"));
 
 server.listen(port, () => {
